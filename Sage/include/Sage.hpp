fb3ada80995b7aaef197aac4574e507f6d0d39c6
Kevin Dean <kadean08@gmail.com>
2020-03-27 15:53:24 -0400

    Complete CMakeLists to create working build and organize directories.

---
+++ Sage/include/Sage.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+//For use by Sage Engine client applications
+
+#include "Sage/Application.hpp"
+#include "Sage/Log.hpp"
+
+//Entry point
+#include "Sage/EntryPoint.hpp"
--- Sage/src/Sage.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//For use by Sage Engine client applications
-
-#include "SageCore/Application.hpp"
-#include "SageCore/Log.hpp"
-
-//Entry point
-#include "SageCore/EntryPoint.hpp"
