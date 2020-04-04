e1ec6baa752ac7d91edfaaeb9fe26453da23eea6
Kevin Dean <kadean08@gmail.com>
2020-04-04 16:05:37 -0400

    Include GLFW and create window interface

---
+++ Sage/include/Sage/Window.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Sagepch.hpp"
+
+#include "Sage/Core.hpp"
+#include "Sage/Events/Event.hpp"
+
+namespace Sage {
+
+    struct WindowProps
+    {
+        std::string Title;
+        unsigned int Width;
+        unsigned int Height;
+    };
+
+    WindowProps buildMyWindowProps(const std::string& title="Sage Engine", unsigned int width = 1280, unsigned int height = 720)
+    {
+        return {title, width, height};
+    }
+
+    //interface representing a desktop window, to be implemented per-platform
+    class Window {
+    public:
+        using EventCallbackFn = std::function<void(Event&)>;
+
+        virtual ~Window() = default;
+
+        virtual void OnUpdate() = 0;
+
+        virtual unsigned int GetWidth() const = 0;
+        virtual unsigned int GetHeight() const = 0;
+
+        //window attributes
+        virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+        virtual void SetVSync(bool enabled) = 0;
+        virtual bool IsVSync() const = 0;
+
+        virtual void* GetNativeWindow() const = 0;
+
+        static Window* Create(const WindowProps& props = buildMyWindowProps()); 
+
+    };
+
+}
